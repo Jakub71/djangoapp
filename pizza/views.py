27edc4e828f8c2e83824a812d1158db97975fc0f
Jakub71 <Jakubjan0208@interia.pl>
2019-09-19 08:45:31 +0200

    apele

--- pizza/views.py
+++ pizza/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
+
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("<h1>Welcome to the jungle!</h1>")
+    # return HttpResponse("<h1>Welcome to the jungle!</h1>")
+    return render(request,'pizza/index.html')
 
 def komunikat(request):
-    return HttpResponse("<h2>Wstawaj samuraju!</h2>")
+    # return HttpResponse("<h2>Wstawaj samuraju!</h2>")
+    return render(request,'pizza/komunikat.html')
 
 def apel(request):
     return HttpResponse("<h3>Padłeś?Powstań!</h3>")
