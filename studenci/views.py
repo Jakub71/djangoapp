f059fc1d8d314c7e417dd47adf08e3036e4fbcfb
Jakub71 <Jakubjan0208@interia.pl>
2019-10-17 08:44:48 +0200

    asdfg

--- studenci/views.py
+++ studenci/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from studenci.models import Miasto, Uczelnia
+
+
 
 def index(request):
     return HttpResponse("<h1>Witaj w aplikacji Studenci!</h1>")
@@ -9,3 +12,24 @@ def index(request):
 def komunikat(request):
     return HttpResponse("<h1>Komunikat!</h1>")
     # return render(request, 'pizza/komunikat.html'
+
+def miasta(request):
+    if request.method == 'POST':
+        nazwa = request.POST.get('nazwa', '')
+        kod = request.POST.get('kod', '')
+        m = Miasto(nazwa=nazwa, kod=kod)
+        m.save()
+
+    miasta = Miasto.objects.all()
+    kontekst = {'miasta': miasta}
+    return render(request, 'studenci/miasta.html', kontekst)
+
+def uczelnie(request):
+    if request.method == 'POST':
+        nazwa = request.POST.get('nazwa', '')
+        n = Uczelnia(nazwa=nazwa, )
+        n.save()
+
+    uczelnie = Uczelnia.objects.all()
+    kontekst = {'uczelnie': uczelnie}
+    return render(request, 'studenci/uczelnie.html', kontekst)
